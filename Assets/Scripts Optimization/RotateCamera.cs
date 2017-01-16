9922f03221d8ff09872d6190ca0c87ce18e218ac
Ivan Tsaklev <tsaklev@gmail.com>
2017-01-16 15:19:32 +0200

    Initial commit

---
+++ Assets/Scripts Optimization/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour 
+{
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("left"))
+		{
+			Camera.main.transform.RotateAround (Vector3.zero, Vector3.up, 50 * Time.deltaTime);
+		} 
+		else if (Input.GetKey ("right"))
+		{
+			Camera.main.transform.RotateAround (Vector3.zero, Vector3.up, -50 * Time.deltaTime);	
+		}
+	}
+}
